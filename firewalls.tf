c7d048e88241f25ab0858464811689356963510b
Andrew <ya.andrew.lo@outlook.com>
2023-04-11 19:36:46 +0800

    firewalls: add allow_masters_range

--- firewalls.tf
+++ firewalls.tf
@@ -8,3 +8,15 @@ resource "google_compute_firewall" "only_internal_traffic" {
 
   source_ranges = ["10.0.0.0/8"]
 }
+
+resource "google_compute_firewall" "allow_masters_range" {
+  name    = "allow-masters-range"
+  network = google_compute_network.gke_vpc.name
+
+  allow {
+    protocol = "all"
+  }
+
+  source_ranges = ["172.16.0.0/16"]
+}
+
