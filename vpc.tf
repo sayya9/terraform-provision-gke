5f26eb065e9f6fa115a3ef0180e0dff515167b7d
Andrew <ya.andrew.lo@outlook.com>
2023-04-11 19:31:25 +0800

    remove cloudresourcemanager

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,3 @@
-resource "google_project_service" "cloudresourcemanager" {
-  service = "cloudresourcemanager.googleapis.com"
-}
-
 resource "google_project_service" "compute" {
   service = "compute.googleapis.com"
 }
